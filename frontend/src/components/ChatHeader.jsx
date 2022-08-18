29b5f530aeac2def9a255198f67b74157ef19c19
Tsogoeva Tatiana <tsogoevats@gmail.com>
2022-08-18 21:51:35 +0300

    find message count

--- frontend/src/components/ChatHeader.jsx
+++ frontend/src/components/ChatHeader.jsx
@@ -4,17 +4,17 @@ import { useSelector } from "react-redux";
 const ChatHeader = () => {
   const channels = useSelector((state) => Object.values(state.channels.entities));
   const currentChannelId = useSelector((state) => state.channels.currentChannelId);
- // const messages = useSelector(messageSelectors.selectAll);
+  const messages = useSelector((state) => Object.values(state.messages.entities));
 
-  const currentChannel = channels.find(({ id }) => id === currentChannelId);
- // const messageCount = messages.filter((message) => console.log(message))
+  const { name } = channels.find(({ id }) => id === currentChannelId);
+  const messageCount = messages.filter(({ channelId }) => channelId === currentChannelId).length;
 
   return (
     <div className="bg-light mb-4 p-3 shadow-sm small">
       <p className="m-0">
-        <b>{`# ${currentChannel.name}`}</b>
+        <b>{`# ${name}`}</b>
       </p>
-      <span className="text-muted">0 сообщений</span>
+      <span className="text-muted">{`${messageCount} сообщений`}</span>
     </div>
   )
 };
