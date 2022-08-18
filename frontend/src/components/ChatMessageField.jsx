8bcb4370b18458217eea80ac01f72019c04530b7
Tsogoeva Tatiana <tsogoevats@gmail.com>
2022-08-18 21:50:00 +0300

    using formik and socket

--- frontend/src/components/ChatMessageField.jsx
+++ frontend/src/components/ChatMessageField.jsx
@@ -1,44 +1,60 @@
-import React, { useState, useRef, useEffect } from "react";
-import { Button, Form, FormControl, InputGroup } from "react-bootstrap";
-import { ArrowRightCircleFill } from "react-bootstrap-icons";
+import React, { useRef, useEffect } from "react";
+import { Button, Form, InputGroup } from "react-bootstrap";
+import { ArrowRightSquare } from "react-bootstrap-icons";
 import { useFormik } from "formik";
 import { useSelector } from "react-redux";
+import { useSocket } from "../hooks/index.js";
 
 const ChatMessageField = () => {
-    const [text, setText] = useState('');
+    const currentChannelId = useSelector((state) => state.channels.currentChannelId);
+    const username = JSON.parse(localStorage.getItem('userId')).username;
+
+    const { sendMessage } = useSocket();
     const inputRef = useRef(null);
 
     useEffect(() => {
         inputRef.current.focus();
     });
 
-    const handleChange = (e) => {
-        setText(e.target.value)
-    };
+    const formik = useFormik({
+        initialValues: {
+            body: '',
+        },
+        onSubmit: (values) => {
+            formik.resetForm();
+            sendMessage({
+                body: values.body,
+                username,
+                channelId: currentChannelId,
+            });
+        },
+    });
 
     return (
         <div className="mt-auto px-5 py-3">
-              <Form className="border rounded" onSubmit={handleSubmit}>
-                <InputGroup>
-                    <FormControl
-                    className="border-0 p-0 ps-2"
-                    name="body"
-                    value={text}
-                    onChange={(e) => handleChange(e)}
-                    placeholder="Введите сообщение..."
-                    aria-label="Новое сообщение"
-                    ref={inputRef}
+              <Form noValidate className="py-1 border rounded-2" onSubmit={formik.handleSubmit}>
+                <Form.Group as={InputGroup} className="has-validation">
+                    <Form.Control
+                        onChange={formik.handleChange}
+                        value={formik.values.body}
+                        name="body"
+                        id="body"
+                        type="text"
+                        ref={inputRef}
+                        className="border-0 p-0 ps-2"
+                        placeholder="Введите сообщение..."
                     />
+                    <Form.Label htmlFor="body" className="visually-hidden">Новое сообщение</Form.Label>
                     <Button
-                    type="submit"
-                    variant="link"
-                    className="mb-1 text-dark"
-                    disabled={!dirty || isSubmitting}
+                        type="submit"
+                        variant="link"
+                        className="mb-1 text-dark"
+                        disabled={formik.values.body.length === 0}
                     >
-                    <ArrowRightCircleFill size={20} />
+                    <ArrowRightSquare size={20} />
                     <span className="visually-hidden">Отправить</span>
                     </Button>
-                </InputGroup>
+                </Form.Group>
               </Form>
         </div>
     )
