d6a1a382ded5a5f5379d478de4763da064fba1b3
Tsogoeva Tatiana <tsogoevats@gmail.com>
2022-08-22 21:59:28 +0300

    add scroll down

--- frontend/src/components/ChatMessagesBox.jsx
+++ frontend/src/components/ChatMessagesBox.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { useSelector } from "react-redux";
 
 const ChatMessagesBox = () => {
     const messages = useSelector((state) => Object.values(state.messages.entities))
     const currentChannelId = useSelector((state) => state.channels.currentChannelId);
-
+    
     const currentChannelMessages = messages.filter((message) => message.channelId === currentChannelId);
 
+    const messagesBoxRef = useRef(null);
+    useEffect(() => {
+        messagesBoxRef.current.scrollTop = messagesBoxRef.current.scrollHeight;
+    });
+
     return (
-        <div id="messages-box" className="chat-messages overflow-auto px-5">
+        <div id="messages-box" className="chat-messages overflow-auto px-5" ref={messagesBoxRef}>
             {currentChannelMessages.map(({ body, username, id }) => (
                 <div key={id} className="text-break mb-2">
                     <b>{username}</b>
