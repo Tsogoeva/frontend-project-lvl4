8db7dcfd6eb7c138839f9ba8ceaf0f8d5ec3c2d1
Tsogoeva Tatiana <tsogoevats@gmail.com>
2022-08-22 21:50:51 +0300

    add modal for adding new channal

--- frontend/src/components/modals/Add.jsx
+++ frontend/src/components/modals/Add.jsx
@@ -1,49 +1,80 @@
-import React, { useEffect, useRef } from 'react';
-import _ from 'lodash';
-import { useFormik } from 'formik';
-import { Modal, FormGroup, FormControl } from 'react-bootstrap';
+import React, { useEffect, useRef } from "react";
+import { useSelector } from "react-redux";
+import { Modal, Form, InputGroup, Button } from "react-bootstrap";
+import { useFormik } from "formik";
+import * as yup from "yup";
 
-const generateOnSubmit = ({ setItems, onHide }) => (values) => {
-  const item = { id: _.uniqueId(), body: values.body };
-  setItems((items) => {
-    items.push(item);
-  });
-  onHide();
-};
+import { useSocket } from "../../hooks/index.js";
 
-const Add = (props) => {
-  const { onHide } = props;
-  const formik = useFormik({
-    onSubmit: generateOnSubmit(props),
-    initialValues: { body: '' },
-  });
-
+const Add = ({ onHide }) => {
+  const channels = useSelector((state) => Object.values(state.channels.entities));
+  const channelNames = channels.map((channel) => channel.name);
+  
   const inputRef = useRef();
   useEffect(() => {
     inputRef.current.focus();
   }, []);
 
+  const { addNewChannel } = useSocket();
+
+  yup.setLocale({
+    mixed: {
+      required: 'Обязательное поле',
+      notOneOf: 'Должно быть уникальным',
+      min: 'от 3 до 20 символов',
+      max: 'от 3 до 20 символов',
+    },
+  });
+
+  const schema = yup.object({
+    name: yup.string().required().min(3).max(20).notOneOf(channelNames),
+  });
+
+  const formik = useFormik({
+    initialValues: { name: '' },
+    validationSchema: schema,
+    onSubmit: ({ name }) => {
+        addNewChannel(name, () => {
+          formik.resetForm();
+          onHide();
+        });
+      },
+    validateOnChange: false,
+    validateOnBlur: false,
+    });
+
   return (
-    <Modal show>
+    <Modal aria-labelledby="contained-modal-title-vcenter" show centered>
       <Modal.Header closeButton onHide={onHide}>
-        <Modal.Title>Add</Modal.Title>
+        <Modal.Title id="contained-modal-title-vcenter">Добавить канал</Modal.Title>
       </Modal.Header>
 
       <Modal.Body>
-        <form onSubmit={formik.handleSubmit}>
-          <FormGroup>
-            <FormControl
+        <Form onSubmit={formik.handleSubmit}>
+          <Form.Group as={InputGroup} className="mb-3 modal-input-block">
+            <Form.Control
               required
               ref={inputRef}
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
-              value={formik.values.body}
-              data-testid="input-body"
-              name="body"
+              value={formik.values.name}
+              isInvalid={!!formik.errors.name}
+              type="text"
+              id="name"
             />
-          </FormGroup>
-          <input type="submit" className="btn btn-primary mt-2" value="submit" />
-        </form>
+            <Form.Label
+              htmlFor="name"
+              className="visually-hidden"
+            >
+              Имя канала
+            </Form.Label>
+            <Form.Control.Feedback type="invalid">{formik.errors.name}</Form.Control.Feedback>
+          </Form.Group>
+          <div className="d-flex justify-content-end">
+              <Button variant="secondary" type="button" className="me-2" onClick={onHide}>Отменить</Button>
+              <Button variant="primary" type="submit" disabled={formik.isSubmitting}>Отправить</Button>
+          </div>
+        </Form>
       </Modal.Body>
     </Modal>
   );
