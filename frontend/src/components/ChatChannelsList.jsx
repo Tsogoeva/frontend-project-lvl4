5d4fd471e1abb214ad6122cc1881214ca698c01a
Tsogoeva Tatiana <tsogoevats@gmail.com>
2022-08-22 21:57:54 +0300

    add chat channel list

--- frontend/src/components/ChatChannelsList.jsx
+++ frontend/src/components/ChatChannelsList.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import {
+  Col,
   Button,
   Nav,
   Dropdown,
   ButtonGroup,
 } from "react-bootstrap";
+import { PlusSquare } from "react-bootstrap-icons";
 import { useDispatch, useSelector } from "react-redux";
 
 import { setCurrentChannelId } from "../slices/channelsSlice.js";
 
-
-const ChatChannelsList = () => {
+const ChatChannelsList = ({ showModal }) => {
     const channels = useSelector((state) => Object.values(state.channels.entities));
     const currentChannelId = useSelector((state) => state.channels.currentChannelId);
+
     const dispatch = useDispatch();
   
     const handleClick = (channelId) => () => {
@@ -20,40 +22,52 @@ const ChatChannelsList = () => {
     };
   
     return (
-      <Nav fill variant="pills" as="ul" className="flex-column px-2">
-        {channels.map(({ id, name, removable }) => {
-          const variant = id === currentChannelId ? 'secondary' : '';
-  
-          return removable ? (
-            <Nav.Item key={id} className="w-100">
-              <Dropdown as={ButtonGroup} className="d-flex">
-  
+      <Col xs={4} md={2} className="border-end pt-5 px-0 bg-light">
+            
+          <div className="d-flex justify-content-between mb-2 ps-4 pe-2">
+            <span>Каналы</span>
+            <Button type="button" onClick={() => showModal('adding')} variant="group-vertical" className="p-0 text-primary">
+              <PlusSquare size={20} /> 
+              <span className="visually-hidden">+</span>    
+            </Button>
+          </div>
+
+        <Nav fill variant="pills" as="ul" className="flex-column px-2">
+          {channels.map(({ id, name, removable }) => {
+            const variant = id === currentChannelId ? 'secondary' : '';
+            console.log(name)
+            
+            return removable ? (
+              <Nav.Item key={id} className="w-100">
+                <Dropdown as={ButtonGroup} className="d-flex">
+
+                  <Button variant={variant} className="w-100 rounded-0 text-start text-truncate" onClick={handleClick(id)}>
+                    <span className="me-1">#</span>
+                    {name}
+                  </Button>
+
+                  <Dropdown.Toggle split variant={variant} className="flex-grow-0">
+                    <span className="visually-hidden">Управление каналом</span>
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu>
+                    <Dropdown.Item onClick={() => showModal('removing', { id, name })}>Удалить</Dropdown.Item>
+                    <Dropdown.Item onClick={() => showModal('renaming', { id, name })}>Переименовать</Dropdown.Item>
+                  </Dropdown.Menu>
+
+                </Dropdown>
+              </Nav.Item>
+            ) : (
+              <Nav.Item key={id}>
                 <Button variant={variant} className="w-100 rounded-0 text-start" onClick={handleClick(id)}>
                   <span className="me-1">#</span>
                   {name}
                 </Button>
-  
-                <Dropdown.Toggle split variant={variant} className="flex-grow-0">
-                  <span className="visually-hidden">Управление каналом</span>
-                </Dropdown.Toggle>
-  
-                {/* <Dropdown.Menu>
-                  <Dropdown.Item onClick={() => showModal('removing', id, name)}>Удалить</Dropdown.Item>
-                  <Dropdown.Item onClick={() => showModal('renaming', id, name)}>Переименовать</Dropdown.Item>
-                </Dropdown.Menu> */}
-  
-              </Dropdown>
-            </Nav.Item>
-          ) : (
-            <Nav.Item key={id}>
-              <Button variant={variant} className="w-100 rounded-0 text-start" onClick={handleClick(id)}>
-                <span className="me-1">#</span>
-                {name}
-              </Button>
-            </Nav.Item>
-          )
-        })}
-      </Nav>
+              </Nav.Item>
+            )
+          })}
+        </Nav>
+      </Col>
     )
   };
 
