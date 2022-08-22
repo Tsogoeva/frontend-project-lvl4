2d880967c094695370c32979e5b0c67eb2634c8a
Tsogoeva Tatiana <tsogoevats@gmail.com>
2022-08-22 21:48:13 +0300

    prepare for initialization

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 //import "bootstrap/dist/js/bootstrap.bundle.min";
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+//import './index.css';
 import App from './App.jsx';
-import reportWebVitals from './reportWebVitals';
+//import initialization from "./init.js";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
---
+++ frontend/src/init.js
@@ -0,0 +1,23 @@
+/* import React from "react";
+import App from "./App.jsx";
+
+const initialization = () => {
+
+    i18n
+    .use(initReactI18next)
+    .init({
+      resources,
+      lng: 'ru',
+      interpolation: {
+        escapeValue: false,
+      },
+    });
+
+  return (
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
+}
+
+export default initialization; */
