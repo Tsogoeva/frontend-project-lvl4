0f1f7e6c6bc07c2fc9afe386aff43d1f7f3b9495
Tsogoeva Tatiana <tsogoevats@gmail.com>
2022-08-24 20:10:23 +0300

    add i18next, openModal state, fix render

--- frontend/src/components/modals/Rename.jsx
+++ frontend/src/components/modals/Rename.jsx
@@ -1,6 +1,9 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
-import { Modal, Form, InputGroup, Button } from "react-bootstrap";
+import { Modal, Form, Button } from "react-bootstrap";
+import { useTranslation } from "react-i18next";
+import { toast } from "react-toastify";
+
 import { useFormik } from "formik";
 import * as yup from "yup";
 
@@ -9,17 +12,26 @@ import { useSocket } from "../../hooks/index.js";
 const Rename = ({ onHide, modalInfo }) => {
   const { name, id } = modalInfo.channelInfo;
   const { setNewChannelName } = useSocket();
+  const { t } = useTranslation();
 
   const channels = useSelector((state) => Object.values(state.channels.entities));
   const channelNames = channels.map((channel) => channel.name);
 
+  const [openModal, setOpenModal] = useState(false);
+
   const inputRef = useRef();
   useEffect(() => {
+    setOpenModal(true);
     inputRef.current.select();
-  }, []);
+  }, [openModal]);
 
   const schema = yup.object({
-    name: yup.string().required().min(3).max(20).notOneOf(channelNames),
+    name: yup
+      .string()
+      .required(t('feedback.required'))
+      .min(3, t('feedback.usernameLength', { min: 3, max: 20 }))
+      .max(20, t('feedback.usernameLength', { min: 3, max: 20 }))
+      .notOneOf(channelNames, t('feedback.uniqueName')),
   });
 
   const formik = useFormik({
@@ -28,44 +40,46 @@ const Rename = ({ onHide, modalInfo }) => {
     onSubmit: ({ name }) => {
         setNewChannelName({ name, id }, () => {
           formik.resetForm();
+          setOpenModal(false);
           onHide();
+          toast.success(t('notices.renameChannel'));
         });
       },
     validateOnChange: false,
     validateOnBlur: false,
-  })
+  });
 
   return (
-    <Modal aria-labelledby="contained-modal-title-vcenter" show centered>
-      <Modal.Header closeButton onHide={onHide}>
-        <Modal.Title id="contained-modal-title-vcenter">Переименовать канал</Modal.Title>
+    <Modal show centered onHide={onHide}>
+      <Modal.Header closeButton>
+        <Modal.Title>{t('modals.rename.title')}</Modal.Title>
       </Modal.Header>
 
       <Modal.Body>
         <Form onSubmit={formik.handleSubmit}>
-          <Form.Group as={InputGroup} className="mb-3 modal-input-block">
+          <Form.Group>
             <Form.Control
-              required
+              className="mb-2"
               ref={inputRef}
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
               value={formik.values.name}
               isInvalid={!!formik.errors.name}
-              type="text"
               id="name"
+              name="name"
             />
             <Form.Label
               htmlFor="name"
               className="visually-hidden"
             >
-              Имя канала
+              {t('modals.rename.channelName')}
             </Form.Label>
             <Form.Control.Feedback type="invalid">{formik.errors.name}</Form.Control.Feedback>
+            <div className="d-flex justify-content-end">
+                <Button variant="secondary" type="button" className="me-2" onClick={onHide}>{t('modals.rename.cancel')}</Button>
+                <Button variant="primary" type="submit" disabled={formik.isSubmitting}>{t('modals.rename.submit')}</Button>
+            </div>
           </Form.Group>
-          <div className="d-flex justify-content-end">
-              <Button variant="secondary" type="button" className="me-2" onClick={onHide}>Отменить</Button>
-              <Button variant="primary" type="submit" disabled={formik.isSubmitting}>Отправить</Button>
-          </div>
         </Form>
       </Modal.Body>
     </Modal>
