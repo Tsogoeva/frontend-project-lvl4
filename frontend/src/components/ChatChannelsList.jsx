2677943b7b6ca3f3dd71d5e72890243544646902
Tsogoeva Tatiana <tsogoevats@gmail.com>
2022-08-24 20:08:21 +0300

    add i18next, leo-profanity filter

--- frontend/src/components/ChatChannelsList.jsx
+++ frontend/src/components/ChatChannelsList.jsx
@@ -8,10 +8,14 @@ import {
 } from "react-bootstrap";
 import { PlusSquare } from "react-bootstrap-icons";
 import { useDispatch, useSelector } from "react-redux";
+import { useTranslation } from "react-i18next";
+import filter from "leo-profanity";
 
 import { setCurrentChannelId } from "../slices/channelsSlice.js";
 
 const ChatChannelsList = ({ showModal }) => {
+    const { t } = useTranslation('translation', { keyPrefix: 'chat.channelList' });
+
     const channels = useSelector((state) => Object.values(state.channels.entities));
     const currentChannelId = useSelector((state) => state.channels.currentChannelId);
 
@@ -25,7 +29,7 @@ const ChatChannelsList = ({ showModal }) => {
       <Col xs={4} md={2} className="border-end pt-5 px-0 bg-light">
             
           <div className="d-flex justify-content-between mb-2 ps-4 pe-2">
-            <span>Каналы</span>
+            <span>{t('header')}</span>
             <Button type="button" onClick={() => showModal('adding')} variant="group-vertical" className="p-0 text-primary">
               <PlusSquare size={20} /> 
               <span className="visually-hidden">+</span>    
@@ -35,7 +39,7 @@ const ChatChannelsList = ({ showModal }) => {
         <Nav fill variant="pills" as="ul" className="flex-column px-2">
           {channels.map(({ id, name, removable }) => {
             const variant = id === currentChannelId ? 'secondary' : '';
-            console.log(name)
+            const filteredName = filter.clean(name);
             
             return removable ? (
               <Nav.Item key={id} className="w-100">
@@ -43,16 +47,16 @@ const ChatChannelsList = ({ showModal }) => {
 
                   <Button variant={variant} className="w-100 rounded-0 text-start text-truncate" onClick={handleClick(id)}>
                     <span className="me-1">#</span>
-                    {name}
+                    {filteredName}
                   </Button>
 
                   <Dropdown.Toggle split variant={variant} className="flex-grow-0">
-                    <span className="visually-hidden">Управление каналом</span>
+                    <span className="visually-hidden">{t('channelControl')}</span>
                   </Dropdown.Toggle>
 
                   <Dropdown.Menu>
-                    <Dropdown.Item onClick={() => showModal('removing', { id, name })}>Удалить</Dropdown.Item>
-                    <Dropdown.Item onClick={() => showModal('renaming', { id, name })}>Переименовать</Dropdown.Item>
+                    <Dropdown.Item onClick={() => showModal('removing', { id, name })}>{t('removeChannel')}</Dropdown.Item>
+                    <Dropdown.Item onClick={() => showModal('renaming', { id, name })}>{t('renameChannel')}</Dropdown.Item>
                   </Dropdown.Menu>
 
                 </Dropdown>
