2601513f019e1c13bf1a8aa8bdf5c2b97a288bc2
Tsogoeva Tatiana <tsogoevats@gmail.com>
2022-08-25 22:19:57 +0300

    remove reportWebVitals

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-// import "bootstrap/dist/js/bootstrap.bundle.min";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
 import init from "./init.js";
-//import reportWebVitals from "./reportWebVitals";
 
 const runApp = async () => {
   const vdom = await init();
@@ -18,8 +16,3 @@ const runApp = async () => {
 };
 
 runApp();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
