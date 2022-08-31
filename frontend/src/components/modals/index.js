60784667bd34d750b38fbe503709dd88c750822b
Tsogoeva Tatiana <tsogoevats@gmail.com>
2022-08-31 19:40:49 +0300

    linting

--- frontend/src/components/modals/index.js
+++ frontend/src/components/modals/index.js
@@ -1,3 +1,5 @@
+/* eslint import/no-anonymous-default-export: [2, {"allowArrowFunction": true}] */
+
 import Add from './Add.jsx';
 import Remove from './Remove.jsx';
 import Rename from './Rename.jsx';
