d71beb808556f8869d27c94a5fcebd304434a09b
Tsogoeva Tatiana <tsogoevats@gmail.com>
2022-08-31 19:31:54 +0300

    add modalsSlice

--- frontend/src/slices/index.js
+++ frontend/src/slices/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import channelsSlice from './channelsSlice.js';
 import messagesSlice from './messagesSlice.js';
+import modalsSlice from './modalsSlice.js';
 
 export default configureStore({
   reducer: {
     channels: channelsSlice,
     messages: messagesSlice,
+    modals: modalsSlice,
   },
 });
---
+++ frontend/src/slices/modalsSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    isOpen: false,
+    type: null,
+    data: null,
+}
+
+const modalsSlice = createSlice({
+  name: 'modals',
+  initialState,
+  reducers: {
+    openModal: (state, { payload }) => {
+        const { type, data } = payload;
+        state.isOpen = true;
+        state.type = type;
+        state.data = data;
+    },
+    closeModal: (state) => {
+        state.isOpen = false;
+        state.type = null;
+        state.data = null;
+    }
+  },
+});
+
+export const { openModal, closeModal } = modalsSlice.actions;
+
+export default modalsSlice.reducer;
